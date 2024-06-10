2d149e13db6c363bdd569c6a015c05fd99ef3a37
Atharv23sm <atharvsm23@gmail.com>
2024-06-10 23:34:42 +0530

    profile picture code updated

--- client/src/component/ProfilePic.jsx
+++ client/src/component/ProfilePic.jsx
@@ -1,11 +1,11 @@
-
+import defaultpp from '../assets/default.png'
 function ProfilePic({ppname}) {
     return (
         <div className='w-52 h-52 border-[1px] flex justify-center items-center overflow-hidden rounded-full'>
             {ppname ?
                 <img src={ppname} alt="" className='h-full' />
                 :
-                <img src='https://static.vecteezy.com/system/resources/thumbnails/009/292/244/small/default-avatar-icon-of-social-media-user-vector.jpg' alt="" className='h-full' />
+                <img src={defaultpp} alt="" className='h-full' />
             }
         </div>
     )
