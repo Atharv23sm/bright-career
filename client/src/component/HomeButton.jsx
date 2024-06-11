a8c53e6266bd6518fe4459d493f066aa97e26637
Atharv23sm <atharvsm23@gmail.com>
2024-06-11 21:18:41 +0530

    minor chnage

--- client/src/component/HomeButton.jsx
+++ client/src/component/HomeButton.jsx
@@ -1,9 +1,9 @@
 import { FaArrowAltCircleUp } from "react-icons/fa"
 import { Link } from "react-router-dom"
 
-function HomeButton({ link, bottom, text }) {
+function HomeButton({ link, text }) {
     return (
-        <Link to={`/${link}`} className={`absolute bottom-${bottom} flex gap-2 items-center z-20 w-max justify-center
+        <Link to={`/${link}`} className={`absolute flex gap-2 items-center z-20 w-max justify-center
     bg-[#00000077] backdrop-blur-sm hover:bg-[#00000099] hover:backdrop-blur p-2 md:p-4 border-[2px]
     border-[#fe0] rounded-full duration-1000 group`}>
             {text} <FaArrowAltCircleUp className='rotate-45 group-hover:rotate-90 duration-500 text-[#fe0]' /></Link>
