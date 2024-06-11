3ce966b55c3877538d5efbeebaba8141e5e1a572
Atharv23sm <atharvsm23@gmail.com>
2024-06-11 21:26:34 +0530

    minor change

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,13 +1,13 @@
 import { useContext, useState } from 'react'
-import { Navigate } from 'react-router-dom'
-import { FaAngleDown, } from 'react-icons/fa'
+import { Link, Navigate } from 'react-router-dom'
+import {FaArrowAltCircleUp , FaAngleDown, } from 'react-icons/fa'
 import Header from '../partials/Header'
 import UserContext from '../context/UserContext'
 import Footer from '../partials/Footer'
 import Card from '../component/Card'
 import home1 from '../assets/home1.jpg'
 import home2 from '../assets/home2.webp'
-import HomeButton from '../component/HomeButton'
+
 
 function Home() {
 
@@ -45,7 +45,10 @@ function Home() {
             <div className='h-screen w-full flex justify-center items-center py-20 md:px-5'>
               <div className='w-max relative flex items-center justify-center'>
                 <div className='absolute p-2 md:p-6 text-center text-[7vw] md:text-[5vw] lg:text-[3vw] z-20'>Ready to assemble your<br />DREAM TEAM?</div>
-                <HomeButton link='hire' text='Hire' className='bottom-4'/>
+                <Link to={`/hire`} className={`absolute bottom-4 flex gap-2 items-center z-20 w-max justify-center
+                  bg-[#00000077] backdrop-blur-sm hover:bg-[#00000099] hover:backdrop-blur p-2 md:p-4 border-[2px]
+                  border-[#fe0] rounded-full duration-1000 group`}>
+                    Hire <FaArrowAltCircleUp className='rotate-45 group-hover:rotate-90 duration-500 text-[#fe0]' /></Link>
                 <div className='relative flex justify-center items-center'>
                   <img src={home1} alt="Landing image"
                     className='md:rounded-2xl' />
@@ -58,7 +61,10 @@ function Home() {
             <div className='py-24 flex justify-center relative'>
               <img src={home2} alt="Landing image" />
               <div className='absolute text-center text-black top-32 text-[20px] leading-[20px] backdrop-blur p-2 rounded-xl bg-[]'>Eager for a career shift <br /> or <br />Seeking standout candidate?</div>
-              <HomeButton link='profile' text='Set your role' className='bottom-32'/>
+              <Link to={`/profile`} className={`absolute bottom-32 flex gap-2 items-center z-20 w-max justify-center
+                  bg-[#00000077] backdrop-blur-sm hover:bg-[#00000099] hover:backdrop-blur p-2 md:p-4 border-[2px]
+                  border-[#fe0] rounded-full duration-1000 group`}>
+                    Set your role <FaArrowAltCircleUp className='rotate-45 group-hover:rotate-90 duration-500 text-[#fe0]' /></Link>
             </div>
         }
       </div>
